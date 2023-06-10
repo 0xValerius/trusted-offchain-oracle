26b8b93850dcbf37548fee1711483f9bdfaa9f19
0xvalerius <0xvalerius@proton.me>
2023-06-11 00:16:34 +0200

    MockOracle: fixed variable naming.

--- src/MockOracle.sol
+++ src/MockOracle.sol
@@ -5,19 +5,15 @@ pragma solidity ^0.8.17;
 import {OracleVerifier} from "../src/OracleVerifier.sol";
 
 contract MockOracle is OracleVerifier {
-    uint256 private _price;
-    string private _mock;
+    uint256 public price;
+    string public text;
 
     function updatePrice(bytes memory data, uint256 timestamp, bytes32 messageHash, bytes memory signature)
         public
         verify(data, timestamp, messageHash, signature)
     {
-        (uint256 price, string memory mock) = abi.decode(data, (uint256, string));
-        _price = price;
-        _mock = mock;
-    }
-
-    function getPrice() public view returns (uint256) {
-        return _price;
+        (uint256 newPrice, string memory newText) = abi.decode(data, (uint256, string));
+        price = newPrice;
+        text = newText;
     }
 }
